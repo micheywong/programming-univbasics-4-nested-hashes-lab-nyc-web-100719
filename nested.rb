07e5b4667865a55d6887c0520cd741659df2cb10
Michelle Wong <michellekwong2@gmail.com>
2019-09-25 01:43:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  new_language = "Assembly"
+  programmer_hash[new_language]
+  new_language= "Assembly"
   programmer_hash[:dennis_ritchie][:languages] = new_language
-  puts new_language
+  new_language
 end
